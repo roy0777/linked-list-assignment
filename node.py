b5e808c6a7596ee3ad02c3d719ac87f8d69219bb
roy0777 <142376653+roy0777@users.noreply.github.com>
2026-05-08 13:42:34 +0300

    basic OOP practice

---
+++ node.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None 
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+
+node1.next = node2  
+node2.next = node3  
+
+print("Switching the linked list:")
+current = node1
+while current is not None:
+    print(current.data)
+    current = current.next
